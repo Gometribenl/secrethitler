6fd2003e8869ffb670a58bac0ace8466e9192a2a
Student Danyon Brink <dbrink2262@student.landstede.nl>
2020-02-24 15:43:09 +0100

    vue werkend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import welcome from './components/welcome.vue';
+
+window.Vue = require('vue');
+
+    Vue.component('welcome', welcome);
+
+    const app = new Vue({
+        el: '#app'
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
         <title>Secret Hitler 503</title>
 
         <!-- Fonts -->
@@ -64,7 +65,8 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center position-ref full-height"> 
+        <script type="text/javascript" src="js/app.js"></script>
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -79,12 +81,12 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Secret Hitler
-                </div>
-
+            <div id="app">
+                <welcome></welcome>
             </div>
+
+            <script type="text/javascript" src="js/app.js"></script>
+
         </div>
     </body>
 </html>
