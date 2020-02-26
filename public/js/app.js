2cedd15c7a0a997599b95448f3d4b3c74459c341
Student Rick Mondria <rmondria0919@student.landstede.nl>
2020-02-26 11:03:36 +0100

    components changes

--- public/js/app.js
+++ public/js/app.js
@@ -19524,7 +19524,7 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", [_c("h1", [_vm._v(" Secret Hitler 503 ")])])
+    return _c("div", [_c("h1", [_vm._v("Secret Hitler")])])
   }
 ]
 render._withStripped = true
--- resources/js/components/welcome.vue
+++ resources/js/components/welcome.vue
@@ -1,6 +1,6 @@
 <template>   
     <div>
-        <h1> Secret Hitler 503 </h1>
+        <h1>Secret Hitler</h1>
     </div>
 </template>
 
